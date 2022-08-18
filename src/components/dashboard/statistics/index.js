8f5ae19f5df9197c569136b5eb90926eb26095ee
Sarah Moradi <moradisarah2001@gamil.com>
2022-08-19 01:26:19 +0430

    corrected path name of imported components

--- src/components/dashboard/statistics/index.js
+++ src/components/dashboard/statistics/index.js
@@ -5,6 +5,7 @@ import { Row } from 'reactstrap'
 const Statistics = () => {
   return (
     <Row className='w-100 d-flex justify-content-center my-4'>
+      {/* fixed path name */}
       <ChartStatistics />
       <FolderStatistics />
     </Row>
